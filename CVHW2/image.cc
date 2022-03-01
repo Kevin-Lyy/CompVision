0bc51f27a1a6565e707e478adecb5e940783070f
KevinLyy <kevin@KevinsOmen.localdomain>
2022-02-28 23:50:08 -0500

    p2 done

--- CVHW2/image.cc
+++ CVHW2/image.cc
@@ -69,73 +69,84 @@ void conRegions(Image *an_image) {
   const int num_rows = an_image->num_rows();
   const int num_columns = an_image->num_columns();
 
-  grayToBin(120,an_image);
+  for(int i = 0;i < num_rows;i++){
+    for(int j = 0;j < num_columns;j++){
+      if(an_image->GetPixel(i,j) < 120){
+        an_image->SetPixel(i,j,0);
+      }
+      else{
+        an_image->SetPixel(i,j,49);
+      }
+    }
+  }
   vector<int> equivList,changedList;
   equivList.push_back(50);
   changedList.push_back(50);
   int top,left,place=0;
-  for(int i = 1;i < num_rows;i++){
-    for(int j = 1;j < num_columns;j++){
-      if(an_image->GetPixel(i,j) == 255){
-        top = an_image->GetPixel(i-1,j);
-        left = an_image->GetPixel(i,j-1);
+    for(int i = 1;i < num_rows;i++){
+      for(int j = 1;j < num_columns;j++){
+        if(an_image->GetPixel(i,j) == 49){
+          top = an_image->GetPixel(i-1,j);
+          left = an_image->GetPixel(i,j-1);
 
-        //CHECK TO THE LEFT AND RIGHT IF THEY ARE BACKGROUND 
-        if(top == 0 && left == 0){
-          an_image->SetPixel(i,j,equivList[place]);
-          equivList.push_back(equivList[place]+1);
-          changedList.push_back(equivList[place]+1);
-          place++;
-        }
-        //IF LEFT AND TOP ARE DIFF VALUES
-        else if(top != left){
-          //TOP IS BACKGROUND
-          if(top == 0 && left != 0){
-            an_image->SetPixel(i,j,left);
+          //CHECK TO THE LEFT AND RIGHT IF THEY ARE BACKGROUND 
+          if(top == 0 && left == 0){
+            an_image->SetPixel(i,j,equivList[place]);
+            equivList.push_back(equivList[place]+1);
+            place++;
           }
-          // LEFT IS BACKGROUND
-          else if(left == 0 && top !=0 ){
+          //IF LEFT AND TOP ARE DIFF VALUES
+          else if(top != left){
+            //TOP IS BACKGROUND
+            if(top == 0 && left != 0){
+              an_image->SetPixel(i,j,left);
+            }
+            // LEFT IS BACKGROUND
+            else if(left == 0 && top !=0 ){
               an_image->SetPixel(i,j,top);
-          }
-          else{
-            if(top < left){
-              an_image->SetPixel(i,j,top);
-              for(int x = 0;x <equivList.size();x++){
-                if(equivList[x] == left){
-                  changedList[x] = top;
-                }
-              }
             }
             else{
-              an_image->SetPixel(i,j,left);
-              for(int x = 0;x <equivList.size();x++){
-                if(equivList[x] == top){
-                  changedList[x] = left;
+              if(left < top){
+                an_image->SetPixel(i,j,left);
+                for(int x = 0; x <equivList.size();x++ ){
+                  if(top == equivList[x]){
+                    equivList[x] = left;
+                  }
+                }
+              }
+              else if (left > top){
+                an_image->SetPixel(i,j,top);
+                for(int x = 0; x <equivList.size();x++ ){
+                  if(left == equivList[x]){
+                    equivList[x] = top;
+                  }
                 }
               }
             }
           }
-          
+          //TOP AND LEFT ARE THE SAME AND ARE NOT BACKROUND BUT DIFF FOREGROUND
+          else if(top == left && top != 0){
+            an_image->SetPixel(i,j,top);
+          }
         }
-        //TOP AND LEFT ARE THE SAME AND ARE NOT BACKROUND BUT DIFF FOREGROUND
-        else if(top == left && top != 0){
-          an_image->SetPixel(i,j,top);
-
-        }
-
-        
       }
+    }  
 
+    for(int i = 0; i < equivList.size();i++){
+      equivList[i] = equivList[equivList[i]-50]; 
     }
-  }  
 
-  for(int i = 0; i < changedList.size();i++){
-    // if(equivList[i] != changedList[i]){
-    //   cout <<"from " << equivList[i] << " to "<< changedList[i] << endl;
-    // }
-    cout << changedList[i] << endl;
-  }
-
+    //SECOND PASS
+    for(int i = 1;i < num_rows;i++){
+        for(int j = 1;j < num_columns;j++){
+          for(int x = 0;x < equivList.size();x++){
+            if(an_image->GetPixel(i,j) ==x+50 ){
+              an_image->SetPixel(i,j,equivList[x]);
+            }
+          }
+        }
+    }
+    
 }
 
 bool ReadImage(const string &filename, Image *an_image) {  

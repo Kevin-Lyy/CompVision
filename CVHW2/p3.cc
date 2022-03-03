499b59d316726da7abea8a79b57b3c4827cfdb71
KevinLyy <kevin@KevinsOmen.localdomain>
2022-03-03 00:39:40 -0500

    moving functions out

--- CVHW2/p3.cc
+++ CVHW2/p3.cc
@@ -23,69 +23,70 @@ void conRegions(Image *an_image) {
       }
     }
   }
-  vector<int> equivList,changedList;
-  equivList.push_back(50);
-  changedList.push_back(50);
-  int top,left,place=0;
+
+  vector<int> equiv_list,changed_list;
+  equiv_list.push_back(50);
+  changed_list.push_back(50);
+  int pixel_above,pixel_left,new_pixel=0;
     for(int i = 1;i < num_rows;i++){
       for(int j = 1;j < num_columns;j++){
         if(an_image->GetPixel(i,j) == 49){
-          top = an_image->GetPixel(i-1,j);
-          left = an_image->GetPixel(i,j-1);
+          pixel_above = an_image->GetPixel(i-1,j);
+          pixel_left = an_image->GetPixel(i,j-1);
 
           //CHECK TO THE LEFT AND RIGHT IF THEY ARE BACKGROUND 
-          if(top == 0 && left == 0){
-            an_image->SetPixel(i,j,equivList[place]);
-            equivList.push_back(equivList[place]+1);
-            place++;
+          if(pixel_above == 0 && pixel_left == 0){
+            an_image->SetPixel(i,j,equiv_list[new_pixel]);
+            equiv_list.push_back(equiv_list[new_pixel]+1);
+            new_pixel++;
           }
           //IF LEFT AND TOP ARE DIFF VALUES
-          else if(top != left){
+          else if(pixel_above != pixel_left){
             //TOP IS BACKGROUND
-            if(top == 0 && left != 0){
-              an_image->SetPixel(i,j,left);
+            if(pixel_above == 0 && pixel_left != 0){
+              an_image->SetPixel(i,j,pixel_left);
             }
             // LEFT IS BACKGROUND
-            else if(left == 0 && top !=0 ){
-              an_image->SetPixel(i,j,top);
+            else if(pixel_left == 0 && pixel_above !=0 ){
+              an_image->SetPixel(i,j,pixel_above);
             }
             else{
-              if(left < top){
-                an_image->SetPixel(i,j,left);
-                for(int x = 0; x <equivList.size();x++ ){
-                  if(top == equivList[x]){
-                    equivList[x] = left;
+              if(pixel_left < pixel_above){
+                an_image->SetPixel(i,j,pixel_left);
+                for(int x = 0; x <equiv_list.size();x++ ){
+                  if(pixel_above == equiv_list[x]){
+                    equiv_list[x] = pixel_left;
                   }
                 }
               }
-              else if (left > top){
-                an_image->SetPixel(i,j,top);
-                for(int x = 0; x <equivList.size();x++ ){
-                  if(left == equivList[x]){
-                    equivList[x] = top;
+              else if (pixel_left > pixel_above){
+                an_image->SetPixel(i,j,pixel_above);
+                for(int x = 0; x <equiv_list.size();x++ ){
+                  if(pixel_left == equiv_list[x]){
+                    equiv_list[x] = pixel_above;
                   }
                 }
               }
             }
           }
           //TOP AND LEFT ARE THE SAME AND ARE NOT BACKROUND BUT DIFF FOREGROUND
-          else if(top == left && top != 0){
-            an_image->SetPixel(i,j,top);
+          else if(pixel_above == pixel_left && pixel_above != 0){
+            an_image->SetPixel(i,j,pixel_above);
           }
         }
       }
     }  
 
-    for(int i = 0; i < equivList.size();i++){
-      equivList[i] = equivList[equivList[i]-50]; 
+    for(int i = 0; i < equiv_list.size();i++){
+      equiv_list[i] = equiv_list[equiv_list[i]-50]; 
     }
 
     //SECOND PASS
     for(int i = 1;i < num_rows;i++){
         for(int j = 1;j < num_columns;j++){
-          for(int x = 0;x < equivList.size();x++){
+          for(int x = 0;x < equiv_list.size();x++){
             if(an_image->GetPixel(i,j) ==x+50 ){
-              an_image->SetPixel(i,j,equivList[x]);
+              an_image->SetPixel(i,j,equiv_list[x]);
             }
           }
         }
@@ -95,73 +96,71 @@ void conRegions(Image *an_image) {
 
 void p3(Image *an_image){
   conRegions(an_image);
-  vector<int> numObjects;
+  vector<int> num_of_objects;
 
   const int num_rows = an_image->num_rows();
   const int num_columns = an_image->num_columns();
-  bool isin=false;
+  bool is_in=false;
 
   for(int i = 1;i < num_rows;i++){
       for(int j = 1;j < num_columns;j++){
-
         if(an_image->GetPixel(i,j) != 0){
-          for(int x = 0;x < numObjects.size();x++){
-            if( an_image->GetPixel(i,j) == numObjects[x]){
-              isin = true;
+          for(int x = 0;x < num_of_objects.size();x++){
+            if( an_image->GetPixel(i,j) == num_of_objects[x]){
+              is_in = true;
             }
           }
-          if(!isin){
-            numObjects.push_back(an_image->GetPixel(i,j));
+          if(!is_in){
+            num_of_objects.push_back(an_image->GetPixel(i,j));
           }
-          isin = false;
+          is_in = false;
         }
       }
   }
 
-  vector<int> areaOfObjects(numObjects.size());
+  vector<int> area_of_objects(num_of_objects.size());
     for(int i = 1;i < num_rows;i++){
         for(int j = 1;j < num_columns;j++){
-          for(int x = 0;x < numObjects.size();x++){
-            if(an_image->GetPixel(i,j) == numObjects[x]){
-              areaOfObjects[x] = areaOfObjects[x]+1;
+          for(int x = 0;x < num_of_objects.size();x++){
+            if(an_image->GetPixel(i,j) == num_of_objects[x]){
+              area_of_objects[x] = area_of_objects[x]+1;
             }
           }
         }
   }
 
-  vector<int> centerColumn(numObjects.size()),centerRow(numObjects.size()),numCol(numObjects.size()),numRow((numObjects.size()));
+  vector<int> center_column(num_of_objects.size()),center_row(num_of_objects.size()),num_of_columns(num_of_objects.size()),num_of_rows((num_of_objects.size()));
     for(int i = 1;i < num_rows;i++){
       for(int j = 1;j < num_columns;j++){
-        for(int x = 0;x < numObjects.size();x++){
-          if(an_image->GetPixel(i,j) == numObjects[x]){
-            centerColumn[x] = centerColumn[x] + j;
-            numCol[x] += 1;
-            centerRow[x] = centerRow[x] + i;
-            numRow[x] += 1;
+        for(int x = 0;x < num_of_objects.size();x++){
+          if(an_image->GetPixel(i,j) == num_of_objects[x]){
+            center_column[x] = center_column[x] + j;
+            num_of_columns[x] += 1;
+            center_row[x] = center_row[x] + i;
+            num_of_rows[x] += 1;
           }
         }
       }
     }
 
-    for(int x = 0;x < numObjects.size();x++){
-      centerColumn[x] = centerColumn[x]/numCol[x];
-      centerRow[x] = centerRow[x]/numRow[x];
+    for(int x = 0;x < num_of_objects.size();x++){
+      center_column[x] = center_column[x]/num_of_columns[x];
+      center_row[x] = center_row[x]/num_of_rows[x];
     }   
 
-    for(int i = 0; i < numObjects.size();i++){
-      //cout << "center of item: " << numObjects[i] << " is (" << centerColumn[i] << "," << centerRow[i]<<")" << endl ;
-      an_image->SetPixel(centerRow[i],centerColumn[i],255);
+    for(int i = 0; i < num_of_objects.size();i++){
+      an_image->SetPixel(center_row[i],center_column[i],255);
     }
 
 
-    vector<int> a(numObjects.size()), b(numObjects.size()), c(numObjects.size()) ;
+    vector<int> a(num_of_objects.size()), b(num_of_objects.size()), c(num_of_objects.size()) ;
     for(int i = 1;i < num_rows;i++){
       for(int j = 1;j < num_columns;j++){
-        for(int x = 0;x < numObjects.size();x++){
-          if(an_image->GetPixel(i,j) == numObjects[x]){
-            a[x] = a[x] + pow((i-centerRow[x]),2);
-            b[x] = b[x] + (i-centerRow[x])*(j-centerColumn[x]);
-            c[x] = c[x] + pow((j-centerColumn[x]),2);
+        for(int x = 0;x < num_of_objects.size();x++){
+          if(an_image->GetPixel(i,j) == num_of_objects[x]){
+            a[x] = a[x] + pow((i-center_row[x]),2);
+            b[x] = b[x] + (i-center_row[x])*(j-center_column[x]);
+            c[x] = c[x] + pow((j-center_column[x]),2);
           }
         }
       }
@@ -170,77 +169,71 @@ void p3(Image *an_image){
       b[i] = 2*b[i];
     }
 
-    vector<double> theta1(numObjects.size());
-    for(int x = 0;x < numObjects.size();x++){
+    vector<double> theta1(num_of_objects.size());
+    for(int x = 0;x < num_of_objects.size();x++){
       theta1[x] = atan2(b[x],a[x]-c[x])/2.0;
     }
 
-    vector<double> e_min(numObjects.size());
-    for(int x = 0;x < numObjects.size();x++){
+    vector<double> e_min(num_of_objects.size());
+    for(int x = 0;x < num_of_objects.size();x++){
       e_min[x] = a[x] * sin(theta1[x]) * sin(theta1[x]) - b[x] * sin(theta1[x]) * cos(theta1[x]) + c[x] * cos(theta1[x]) * cos(theta1[x]);
     }
     
-    vector<double> theta2(numObjects.size());
-    for(int x = 0;x < numObjects.size();x++){
+    vector<double> theta2(num_of_objects.size());
+    for(int x = 0;x < num_of_objects.size();x++){
       theta2[x] = theta2[x] + M_PI/2.0;
     }
 
-    vector<double> e_max(numObjects.size());
-    for(int x = 0;x < numObjects.size();x++){
+    vector<double> e_max(num_of_objects.size());
+    for(int x = 0;x < num_of_objects.size();x++){
       e_max[x] = a[x] * sin(theta2[x]) * sin(theta2[x]) - b[x] * sin(theta2[x]) * cos(theta2[x]) + c[x] * cos(theta2[x]) * cos(theta2[x]);
     }
 
-    vector<double> roundedness(numObjects.size());
-    for(int x = 0;x < numObjects.size();x++){
+    vector<double> roundedness(num_of_objects.size());
+    for(int x = 0;x < num_of_objects.size();x++){
       roundedness[x] = e_min[x]/e_max[x];
     }
 
-    vector<double> theta_degrees(numObjects.size());
-    for(int x = 0;x < numObjects.size();x++){
+    vector<double> theta_degrees(num_of_objects.size());
+    for(int x = 0;x < num_of_objects.size();x++){
       theta_degrees[x] = 180.0 * theta1[x]/M_PI;
     }
     //orentation is pi/2 - theta 
-    vector<double> orientation(numObjects.size());
-    for(int x = 0;x < numObjects.size();x++){
+    vector<double> orientation(num_of_objects.size());
+    for(int x = 0;x < num_of_objects.size();x++){
       orientation[x] = 90.0 - theta_degrees[x];
     }
 
-    for(int i = 0; i < numObjects.size();i++){
-      cout << numObjects[i] << " ";
-      cout << centerRow[i] << " ";
-      cout << centerColumn[i] << " ";
-      cout << e_min[i] << " ";
-      cout << areaOfObjects[i] << " ";
-      cout << roundedness[i] << " ";
-      cout << orientation[i] << " ";
-      cout << endl;
-    }
-
-    //equation of the line 
-    //xsin(0) - ycos(0) + p  = 0 
-    //use center x y to find p 
+    // for(int i = 0; i < num_of_objects.size();i++){
+    //   cout << num_of_objects[i] << " ";
+    //   cout << center_row[i] << " ";
+    //   cout << center_column[i] << " ";
+    //   cout << e_min[i] << " ";
+    //   cout << area_of_objects[i] << " ";
+    //   cout << roundedness[i] << " ";
+    //   cout << orientation[i] << " ";
+    //   cout << endl;
+    // }
     
-    vector<double> rho(numObjects.size());
-    for(int i = 0;i < numObjects.size();i++){
-      rho[i] = centerColumn[i]*cos(theta1[i]) - centerRow[i]*sin(theta1[i]);
+    vector<double> rho(num_of_objects.size());
+    for(int i = 0;i < num_of_objects.size();i++){
+      rho[i] = center_column[i]*cos(theta1[i]) - center_row[i]*sin(theta1[i]);
       cout << rho[i] << endl;
     }
 
-    vector<int> newX(numObjects.size()), newY(numObjects.size());
-    for(int i = 0;i < numObjects.size();i++){
-      newX[i] = centerRow[i] + 30;
-      newY[i] = (newX[i] * sin(theta1[i]) + rho[i])/cos(theta1[i]);
+    vector<int> new_x(num_of_objects.size()), new_y(num_of_objects.size());
+    for(int i = 0;i < num_of_objects.size();i++){
+      new_x[i] = center_row[i] + 30;
+      new_y[i] = (new_x[i] * sin(theta1[i]) + rho[i])/cos(theta1[i]);
      
-      // cout << newX[i] << endl;
-      // cout << newY[i] << endl;
+
     }
 
-    for(int i = 0;i < numObjects.size();i++){
-      DrawLine(centerRow[i],centerColumn[i],newX[i],newY[i],255,an_image);
+    for(int i = 0;i < num_of_objects.size();i++){
+      DrawLine(center_row[i],center_column[i],new_x[i],new_y[i],255,an_image);
     }
-    //(int x0, int y0, int x1, int y1, int color,Image *an_image)
 
-    for(int i = 0;i < numObjects.size();i++){
+    for(int i = 0;i < num_of_objects.size();i++){
       cout << theta1[i] << endl;
     }
   

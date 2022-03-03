7246afbfd55a2f6aece55d6103a1e1b924577f6a
KevinLyy <kevin@KevinsOmen.localdomain>
2022-03-02 22:12:48 -0500

    p3 done

--- CVHW2/image.cc
+++ CVHW2/image.cc
@@ -176,117 +176,131 @@ void p3(Image *an_image){
   }
 
   vector<int> areaOfObjects(numObjects.size());
-  for(int i = 1;i < num_rows;i++){
-      for(int j = 1;j < num_columns;j++){
-        for(int x = 0;x < numObjects.size();x++){
-          if(an_image->GetPixel(i,j) == numObjects[x]){
-            areaOfObjects[x] = areaOfObjects[x]+1;
+    for(int i = 1;i < num_rows;i++){
+        for(int j = 1;j < num_columns;j++){
+          for(int x = 0;x < numObjects.size();x++){
+            if(an_image->GetPixel(i,j) == numObjects[x]){
+              areaOfObjects[x] = areaOfObjects[x]+1;
+            }
           }
         }
-      }
   }
 
   vector<int> centerColumn(numObjects.size()),centerRow(numObjects.size()),numCol(numObjects.size()),numRow((numObjects.size()));
-  for(int i = 1;i < num_rows;i++){
-    for(int j = 1;j < num_columns;j++){
-      for(int x = 0;x < numObjects.size();x++){
-        if(an_image->GetPixel(i,j) == numObjects[x]){
-          centerColumn[x] = centerColumn[x] + j;
-          numCol[x] += 1;
-          centerRow[x] = centerRow[x] + i;
-          numRow[x] += 1;
-        }
-      }
-    }
-  }
-
-  for(int x = 0;x < numObjects.size();x++){
-    centerColumn[x] = centerColumn[x]/numCol[x];
-    centerRow[x] = centerRow[x]/numRow[x];
-  }   
-
-  for(int i = 0; i < numObjects.size();i++){
-    //cout << "center of item: " << numObjects[i] << " is (" << centerColumn[i] << "," << centerRow[i]<<")" << endl ;
-    an_image->SetPixel(centerRow[i],centerColumn[i],255);
-  }
-
-
-  vector<int> a(numObjects.size()), b(numObjects.size()), c(numObjects.size()) ;
-  for(int i = 1;i < num_rows;i++){
-    for(int j = 1;j < num_columns;j++){
-      for(int x = 0;x < numObjects.size();x++){
-        if(an_image->GetPixel(i,j) == numObjects[x]){
-          a[x] = a[x] + pow((i-centerColumn[x]),2);
-          b[x] = b[x] + (i-centerColumn[x])*(j-centerRow[x]);
-          c[x] = c[x] + pow((j-centerRow[x]),2);
-        }
-      }
-    }
-  }
-  for(int i = 0;i < b.size();i++){
-    b[i] = 2*b[i];
-  }
-
-  vector<double> theta1(numObjects.size());
-  for(int x = 0;x < numObjects.size();x++){
-    theta1[x] = atan2(b[x],a[x]-c[x])/2.0;
-  }
-
-  vector<double> e_min(numObjects.size());
-  for(int x = 0;x < numObjects.size();x++){
-    e_min[x] = a[x] * sin(theta1[x]) * sin(theta1[x]) - b[x]*sin(theta1[x])*cos(theta1[x]) + c[x]*cos(theta1[x])*cos(theta1[x]);
-  }
-  
-  vector<double> theta2(numObjects.size());
-  for(int x = 0;x < numObjects.size();x++){
-    theta2[x] = theta2[x] + M_PI/2.0;
-  }
-
-  vector<double> e_max(numObjects.size());
-  for(int x = 0;x < numObjects.size();x++){
-    e_max[x] = a[x] * sin(theta2[x]) * sin(theta2[x]) - b[x]*sin(theta2[x])*cos(theta2[x]) + c[x]*cos(theta2[x])*cos(theta2[x]);
-  }
-
-  vector<double> roundedness(numObjects.size());
-  for(int x = 0;x < numObjects.size();x++){
-    roundedness[x] = e_min[x]/e_max[x];
-  }
-
-  vector<double> theta_degrees(numObjects.size());
-  for(int x = 0;x < numObjects.size();x++){
-    theta_degrees[x] = 180.0* theta1[x]/M_PI;
-  }
-
-  vector<double> orientation(numObjects.size());
-  for(int x = 0;x < numObjects.size();x++){
-    orientation[x] = 90.0 - theta_degrees[x];
-  }
-
-  for(int i = 0; i < numObjects.size();i++){
-    cout << numObjects[i] << " ";
-    cout << centerRow[i] << " ";
-    cout << centerColumn[i] << " ";
-    cout << e_min[i] << " ";
-    cout << areaOfObjects[i] << " ";
-    cout << roundedness[i] << " ";
-    cout << orientation[i] << " ";
-    cout << endl;
-  }
-  //equation of the line 
-  //xsin(0) - ycos(0) + p  = 0 
-  //use center x y to find p 
-  for(int i = 1;i < num_rows;i++){
-    for(int j = 1;j < num_columns;j++){
-      for(int x = 0;x < numObjects.size();x++){
-        if(an_image->GetPixel(i,j) == numObjects[x]){
-          if( ((i-centerColumn[x]) * sin(theta1[x]) - (j-centerRow[x]) * cos(theta1[x])) == 0){
-            an_image->SetPixel(i,j,255);
+    for(int i = 1;i < num_rows;i++){
+      for(int j = 1;j < num_columns;j++){
+        for(int x = 0;x < numObjects.size();x++){
+          if(an_image->GetPixel(i,j) == numObjects[x]){
+            centerColumn[x] = centerColumn[x] + j;
+            numCol[x] += 1;
+            centerRow[x] = centerRow[x] + i;
+            numRow[x] += 1;
           }
         }
       }
     }
-  }
-//orentation is pi/2 - theta 
+
+    for(int x = 0;x < numObjects.size();x++){
+      centerColumn[x] = centerColumn[x]/numCol[x];
+      centerRow[x] = centerRow[x]/numRow[x];
+    }   
+
+    for(int i = 0; i < numObjects.size();i++){
+      //cout << "center of item: " << numObjects[i] << " is (" << centerColumn[i] << "," << centerRow[i]<<")" << endl ;
+      an_image->SetPixel(centerRow[i],centerColumn[i],255);
+    }
+
+
+    vector<int> a(numObjects.size()), b(numObjects.size()), c(numObjects.size()) ;
+    for(int i = 1;i < num_rows;i++){
+      for(int j = 1;j < num_columns;j++){
+        for(int x = 0;x < numObjects.size();x++){
+          if(an_image->GetPixel(i,j) == numObjects[x]){
+            a[x] = a[x] + pow((i-centerRow[x]),2);
+            b[x] = b[x] + (i-centerRow[x])*(j-centerColumn[x]);
+            c[x] = c[x] + pow((j-centerColumn[x]),2);
+          }
+        }
+      }
+    }
+    for(int i = 0;i < b.size();i++){
+      b[i] = 2*b[i];
+    }
+
+    vector<double> theta1(numObjects.size());
+    for(int x = 0;x < numObjects.size();x++){
+      theta1[x] = atan2(b[x],a[x]-c[x])/2.0;
+    }
+
+    vector<double> e_min(numObjects.size());
+    for(int x = 0;x < numObjects.size();x++){
+      e_min[x] = a[x] * sin(theta1[x]) * sin(theta1[x]) - b[x] * sin(theta1[x]) * cos(theta1[x]) + c[x] * cos(theta1[x]) * cos(theta1[x]);
+    }
+    
+    vector<double> theta2(numObjects.size());
+    for(int x = 0;x < numObjects.size();x++){
+      theta2[x] = theta2[x] + M_PI/2.0;
+    }
+
+    vector<double> e_max(numObjects.size());
+    for(int x = 0;x < numObjects.size();x++){
+      e_max[x] = a[x] * sin(theta2[x]) * sin(theta2[x]) - b[x] * sin(theta2[x]) * cos(theta2[x]) + c[x] * cos(theta2[x]) * cos(theta2[x]);
+    }
+
+    vector<double> roundedness(numObjects.size());
+    for(int x = 0;x < numObjects.size();x++){
+      roundedness[x] = e_min[x]/e_max[x];
+    }
+
+    vector<double> theta_degrees(numObjects.size());
+    for(int x = 0;x < numObjects.size();x++){
+      theta_degrees[x] = 180.0 * theta1[x]/M_PI;
+    }
+    //orentation is pi/2 - theta 
+    vector<double> orientation(numObjects.size());
+    for(int x = 0;x < numObjects.size();x++){
+      orientation[x] = 90.0 - theta_degrees[x];
+    }
+
+    for(int i = 0; i < numObjects.size();i++){
+      cout << numObjects[i] << " ";
+      cout << centerRow[i] << " ";
+      cout << centerColumn[i] << " ";
+      cout << e_min[i] << " ";
+      cout << areaOfObjects[i] << " ";
+      cout << roundedness[i] << " ";
+      cout << orientation[i] << " ";
+      cout << endl;
+    }
+
+    //equation of the line 
+    //xsin(0) - ycos(0) + p  = 0 
+    //use center x y to find p 
+    
+    vector<double> rho(numObjects.size());
+    for(int i = 0;i < numObjects.size();i++){
+      rho[i] = centerColumn[i]*cos(theta1[i]) - centerRow[i]*sin(theta1[i]);
+      cout << rho[i] << endl;
+    }
+
+    vector<int> newX(numObjects.size()), newY(numObjects.size());
+    for(int i = 0;i < numObjects.size();i++){
+      newX[i] = centerRow[i] + 30;
+      newY[i] = (newX[i] * sin(theta1[i]) + rho[i])/cos(theta1[i]);
+     
+      // cout << newX[i] << endl;
+      // cout << newY[i] << endl;
+    }
+
+    for(int i = 0;i < numObjects.size();i++){
+      DrawLine(centerRow[i],centerColumn[i],newX[i],newY[i],255,an_image);
+    }
+    //(int x0, int y0, int x1, int y1, int color,Image *an_image)
+
+    for(int i = 0;i < numObjects.size();i++){
+      cout << theta1[i] << endl;
+    }
+  
 }
 
 bool ReadImage(const string &filename, Image *an_image) {  

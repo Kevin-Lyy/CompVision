e27440a341a8b5f93f75c04205e6a78b0e889db7
KevinLyy <kevin@KevinsOmen.localdomain>
2022-04-25 01:49:22 -0400

    start mosaic

--- CVFinal/threshold.py
+++ CVFinal/threshold.py
@@ -1,20 +1,13 @@
-# organizing imports
 import cv2
-import numpy as np
- 
-image1 = cv2.imread("CompVision/CVFinal/images/291.jpg")
- 
- 
-
+image1 = cv2.imread("images/8.jpg")
 thresh_val = 70
 image1[image1 < thresh_val] = 0
 
 ret, thresh3 = cv2.threshold(image1, 250, 255, cv2.THRESH_TRUNC)
 
- 
-cv2.imshow('Truncated Threshold', thresh3)
+cv2.imwrite("threshold.jpg", thresh3)
+#cv2.imshow('Truncated Threshold', thresh3)
 
-   
 # De-allocate any associated memory usage 
 if cv2.waitKey(0) & 0xff == 27:
     cv2.destroyAllWindows()

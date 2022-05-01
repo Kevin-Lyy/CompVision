069481808dba8255825e5b704cb79f71eb8f9a57
KevinLyy <kevin@KevinsOmen.localdomain>
2022-05-01 00:39:46 -0400

    pixel-y

--- CVFinal/mosaic.py
+++ CVFinal/mosaic.py
@@ -1,43 +1,15 @@
 import cv2
 import numpy as np
 import imutils
+from PIL import Image
+import matplotlib.pyplot as plt
 
-image1 = cv2.imread("threshold.jpg")
-mosaicX = 50
-mosaicY = 50
+def convertToPixels(image,size):
+    img = Image.open("threshold.jpg")
+    pixelated = img.resize(size,Image.BILINEAR)
+    sizeUp = pixelated.resize(img.size,Image.NEAREST)
+    sizeUp.save("clean.jpg")
 
-lego1 = cv2.imread("302104.jpg")
-def trimage(img, border = 1):
-    # https://stackoverflow.com/a/49907762
-    # width of border around map, will not exceed what exists
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    coords = cv2.findNonZero(gray) # find map pixels 
-    x, y, w, h = cv2.boundingRect(coords) # find minimum spanning bounding box
+convertToPixels(image="threshold.jpg",size=(50,50))
 
-    # fix border  
-    if x - border >= 0:
-        x1 = x - border
-    else: 
-        x1 = 0
-
-    if x + w + border <= img.shape[1]:
-        x2 = x + w + border
-    else:
-        x2 = img.shape[1]
-
-    if y - border >= 0:
-        y1 = y - border
-    else: 
-        y1 = 0
-
-    if y + h + border <= img.shape[0]:
-        y2 = y + h + border
-    else:
-        y2 = img.shape[0]
-
-    return img[y1:y2, x1:x2]
-
-trimage(lego1,1)
-
-cv2.imwrite("clean.jpg", lego1)
